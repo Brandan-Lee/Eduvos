785666a7d625cf5de83d71416c8bfa1fc559ae93
Brandan-Lee <brandan.sherbrooke@gmail.com>
2025-11-02 20:53:15 +0200

    Finalized version for the libary system

--- Java/Datastructures and Algorithms/SmartBookBorrowingSystem/src/model/BookInformation.java
+++ Java/Datastructures and Algorithms/SmartBookBorrowingSystem/src/model/BookInformation.java
@@ -1,39 +1,68 @@
 
 package model;
 
+/**
+ * Model class that stores information about a book
+ * @author brand
+ */
+
 import data.LinkedQueue;
 
 public class BookInformation {
     
+    //the total and available copies of the book
     private int totalCopies, availableCopies;
+    //the waitlist of users for the book that uses the self-made linked queue data structure
     private LinkedQueue<String> waitList;
     
+    /**
+     * The constructor for the books information
+     * @param totalCopies the total copies that the library has of said book
+     */
     public BookInformation(int totalCopies) {
         this.totalCopies = totalCopies;
         this.availableCopies = totalCopies;
         this.waitList = new LinkedQueue<>();
     }
     
+    /**
+     * Get the amount of available copies for the book
+     * @return an int for the amount of available copies
+     */
     public int getAvailableCopies() {
         return availableCopies;
     }
     
+    /**
+     * Reduce the amount of available copies if available copies for the book is available
+     */
     public void decrementAvailableCopies() {
         if (availableCopies > 0) {
             availableCopies--;
         }
     }
     
+    /**
+     * Add available copies available for borrow for the book if the available copies does not exceed the total copies amount for the book
+     */
     public void incrementAvailableCopies() {
         if (availableCopies < totalCopies) {
             availableCopies++;
         }
     }
     
+    /**
+     * Get the wait list of users that is waiting to borrow the book from the library
+     * @return a linked queue of the users that want to borrow the book
+     */
     public LinkedQueue<String> getWaitList() {
         return waitList;
     }
     
+    /**
+     * Display the model class's information
+     * @return a string that stores the amount of available copies, the total copies and the size of the wait list for the book
+     */
     @Override
     public String toString() {
         return "Available: " + availableCopies
--- Java/Datastructures and Algorithms/SmartBookBorrowingSystem/src/ui/LibrarySystemUI.java
+++ Java/Datastructures and Algorithms/SmartBookBorrowingSystem/src/ui/LibrarySystemUI.java
@@ -1,19 +1,33 @@
 package ui;
 
+/**
+ * UI class for the Library System
+ * @author brand
+ */
+
 import core.LibrarySystem;
 import java.util.Scanner;
 
 public class LibrarySystemUI {
     
+    //the library system
     private static LibrarySystem system;
+    //Java API that will be used to receive input from the user
     private static Scanner scanner;
+    //helper method to check if the system is still running or not
     private static boolean isRunning;
     
+    /**
+     * Constructor for the library systems UI. Creates a new library system and scanner object
+     */
     public LibrarySystemUI() {
         this.system = new LibrarySystem();
         this.scanner = new Scanner(System.in);
     }
     
+    /**
+     * Start the library Systems UI
+     */
     public void start() {
         isRunning = true;
         showWelcomeMessage();
@@ -24,9 +38,15 @@ public class LibrarySystemUI {
         } while (isRunning);
     }
     
+    /**
+     * A helper method to get a non-empty string from the user
+     * @param prompt the type of string that we want from the user
+     * @return the non-empty string
+     */
     private String getNonEmptyString(String prompt) {
         String input;
         
+        //keep looping until the user enters a non empty string
         do {
             System.out.println(prompt);
             input = scanner.nextLine().trim();
@@ -39,12 +59,19 @@ public class LibrarySystemUI {
         return input;
     }
     
+    /**
+     * A helper method that ensures that a positive integer is received from the user
+     * @param prompt the type of integer that we want from the user
+     * @return the positive integer
+     */
     private int getPositiveInteger(String prompt) {
         int parsedInt = -1;
         
+        //keep looping until the user enters a postive integer
         while (parsedInt <= 0) {
             System.out.println(prompt);
             
+            //Ensure that parsing the string to an integer is successfull and doesn't break the system
             try {
                String input = scanner.nextLine().trim();
                parsedInt = Integer.parseInt(input);
@@ -59,13 +86,19 @@ public class LibrarySystemUI {
         
         return parsedInt;
     }
-     
+    
+    /**
+     * Greets the user with a welcome message when starting the UI
+     */
     private void showWelcomeMessage() {
         System.out.println("===============================================");
         System.out.println("  WELCOME TO THE LIBRARY SYSTEM");
         System.out.println("===============================================");
     }
     
+    /**
+     * Displays the menu to the user
+     */
     private void showMenu() {
         System.out.println("\nMenu:");
         System.out.println("Please select an option: ");
@@ -79,6 +112,9 @@ public class LibrarySystemUI {
         System.out.print("Enter your choice (1 - 7): ");
     }
     
+    /**
+     * A helper method to process the positive integers entered by the user
+     */
     private void processUserInput() {
         String choice = scanner.nextLine().trim();
             
@@ -109,6 +145,9 @@ public class LibrarySystemUI {
             }
     }
     
+    /**
+     * Allows a book to be added to the library systems inventory
+     */
     private void addBookOperation() {
         String title = getNonEmptyString("Please enter the title of the book: ");
         int copies = getPositiveInteger("Please enter the amount of copies available: ");
@@ -119,6 +158,9 @@ public class LibrarySystemUI {
                 + "Has been successfully added to the library");
     }
     
+    /**
+     *Allow a user to borrow a book from the library. Output is a predefined string calculated in the library systems class
+     */
     private void borrowBookOperation() {
         String title = getNonEmptyString("Please enter the title for the book to be borrowed: ");
         String userId = getNonEmptyString("Please enter the Users ID: ");
@@ -126,6 +168,9 @@ public class LibrarySystemUI {
         System.out.println("\n" + result);
     }
     
+    /**
+     * Allows a user to return a book to the library. Output is a predefined string calculated in the library systems class
+     */
     private void returnBookOperation() {
         String userId = getNonEmptyString("Please enter the User ID that is returning a book: ");
         String title = getNonEmptyString("Please enter the title of the book that they are returning: ");
@@ -133,24 +178,36 @@ public class LibrarySystemUI {
         System.out.println("\n" + result);
     }
     
+    /**
+     * Display the wait list for a certain book in the library
+     */
     private void displayBookWaitListOperation() {
         String title = getNonEmptyString("Enter the title of the book: ");
         String result = system.displayBookWaitList(title);
         System.out.println("\nWaitlist for " + title + "\n" + result);
     }
     
+    /**
+     * Display all the books that the library has borrowed out
+     */
     private void displayBorrowedBooksOperation() {
         System.out.println("\nDisplaying Borrowed Books: \n");
         String result = system.displayBorrowedBooks();
         System.out.println(result);
     }
     
+    /**
+     * Display the libraries inventory
+     */
     private void displayInventoryOperation() {
         System.out.println("\nDisplaying Library Inventory: \n");
         String result = system.displayInventory();
         System.out.println(result);
     }
     
+    /**
+     * Exit the system
+     */
     private void exit() {
         isRunning = false;
         System.out.println("\nThank you for using the Library System. Goodbye");
